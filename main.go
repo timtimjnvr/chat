9461733347424fa481e5de96816f3c09d6858374
Timothee Janvier | AT Internet <timothee.janvier@atinternet.com>
2023-01-28 10:15:10 +0100

    changes(main): refactored main and create an orchestrator function responsible of handling differents IO

--- main.go
+++ main.go
@@ -2,17 +2,13 @@ package main
 
 import (
 	"chat/conn"
-	"chat/crdt"
 	"chat/linked"
 	"chat/node"
 	parsestdin "chat/parsestdin"
 	"flag"
-	"github.com/google/uuid"
 	"log"
-	"net"
 	"os"
 	"os/signal"
-	"strconv"
 	"sync"
 	"syscall"
 )
@@ -35,19 +31,19 @@ func main() {
 	flag.Parse()
 
 	var (
-		currentChat   = crdt.NewChat(*myNamePtr)
 		nodes         = linked.NewList()
 		chats         = linked.NewList()
 		sigc          = make(chan os.Signal, 1)
 		shutdown      = make(chan struct{})
 		portAccept    = *myPortPtr
 		addressAccept = *myAddrPtr
+		wgOrchestrate = sync.WaitGroup{}
 		wgListen      = sync.WaitGroup{}
 		wgReadStdin   = sync.WaitGroup{}
 
-		stdin           = make(chan string, maxMessagesStdin)
+		stdin           = make(chan []byte, maxMessagesStdin)
+		fromConnections = make(chan []byte, maxSimultaneousConnections)
 		newNodes        = make(chan *node.Node, maxSimultaneousConnections)
-		connectionsDone = make(chan uuid.UUID, maxSimultaneousConnections)
 	)
 
 	defer func() {
@@ -63,108 +59,22 @@ func main() {
 		syscall.SIGTERM,
 		syscall.SIGQUIT)
 
-	_ = chats.Add(currentChat)
 	wgListen.Add(1)
 	go conn.ListenAndServe(&wgListen, newNodes, shutdown, transportProtocol, addressAccept, portAccept)
 
 	wgReadStdin.Add(1)
 	go parsestdin.ReadStdin(&wgReadStdin, stdin, shutdown)
 
+	wgOrchestrate.Add(1)
+	go orchestrate(&wgOrchestrate, myNamePtr, stdin, fromConnections, newNodes, chats, nodes, shutdown)
+
+	// go display(chats, refresh <-chan uuid.UUID)
+
 	for {
 		select {
 		case <-sigc:
 			close(shutdown)
 			return
-
-		/* Save the connection and handle connection*/
-		case newNode := <-newNodes:
-			nodes.Add(newNode)
-			newNode.Business.Wg.Add(1)
-			go conn.HandleConnection(newNode, connectionsDone, newNode.Business.Shutdown)
-
-		/* Node done */
-		case id := <-connectionsDone:
-			nodes.Delete(id)
-
-		/* Command input */
-		case line := <-stdin:
-			cmd, err := parsestdin.NewCommand(line)
-			if err != nil {
-				log.Println("[ERROR] ", err)
-			}
-
-			args := cmd.GetCommandArgs()
-
-			switch typology := cmd.GetCommandType(); typology {
-			case parsestdin.CreateChatCommandType:
-				var (
-					chatName = args[parsestdin.ChatRoomArg]
-					newChat  = crdt.NewChat(chatName)
-				)
-
-				chats.Add(newChat)
-
-			case parsestdin.ConnectCommandType:
-				var (
-					addr     = args[parsestdin.AddrArg]
-					chatRoom = args[parsestdin.ChatRoomArg]
-					newConn  net.Conn
-					pt       int
-				)
-
-				pt, err = strconv.Atoi(args[parsestdin.PortArg])
-				if err != nil {
-					log.Println(err)
-				}
-
-				if addr == localhost || addr == localhostDecimalPointed {
-					addr = ""
-				}
-
-				/* Open connection */
-				newConn, err = conn.OpenConnection(transportProtocol, addr, pt)
-				if err != nil {
-					log.Println("[ERROR] ", err)
-					continue
-				}
-
-				/* Saves the new connection into newNode List */
-				newNode := node.NewNode(newConn)
-				newNode.Business.Wg.Add(1)
-				go conn.HandleConnection(newNode, connectionsDone, newNode.Business.Shutdown)
-
-				/* Add the newNode to chatRoom */
-				if chatRoom == *myNamePtr {
-					currentChat.AddNode(newNode)
-				}
-
-				/* sync -> add the new newNode to other nodes */
-
-			case parsestdin.MsgCommandType:
-				content := args[parsestdin.MessageArg]
-				if currentChat == nil {
-					log.Println(noDiscussionSelected)
-					continue
-				}
-
-				/* Add the message to discussion */
-				message := crdt.NewMessage(*myNamePtr, content)
-				currentChat.AddMessage(message)
-
-				syncMessage := crdt.NewOperation(crdt.AddMessage, message.ToRunes())
-				bytesSyncMessage := []byte(string(syncMessage.ToRunes()))
-				currentChat.Send(bytesSyncMessage)
-
-			case parsestdin.CloseCommandType:
-				/* TODO
-				leave discussion and gracefully shutdown connection with all nodes
-				*/
-
-			case parsestdin.QuitCommandType:
-				/* TODO
-				leave all discussions and gracefully shutdown connection with all nodes
-				*/
-			}
 		}
 	}
 }
