2d16e9c445c7ceb7e93fe0990268d6f5d33f4d52
Timothée Janvier <57535343+timtimjnvr@users.noreply.github.com>
2023-02-18 12:27:53 +0100

    fix(conn_test): fix never ending tests (#20)
    * fix(conn): close listener 
    
    * changes: use ListenAndServe to create conn

--- conn/conn_test.go
+++ conn/conn_test.go
@@ -59,9 +59,12 @@ func TestReadConn(t *testing.T) {
 	var (
 		maxTestDuration = 3 * time.Second
 		wgReader        = sync.WaitGroup{}
-		wgSender        = sync.WaitGroup{}
 		messages        = make(chan []byte, reader.MaxMessageSize)
+		wgListen        = sync.WaitGroup{}
 		shutdown        = make(chan struct{}, 0)
+		lock            = sync.Mutex{}
+		isListening     = sync.NewCond(&lock)
+		newConnections  = make(chan net.Conn, MaxSimultaneousConnections)
 	)
 
 	var testData = []string{
@@ -71,26 +74,10 @@ func TestReadConn(t *testing.T) {
 	}
 
 	// sender
-	wgSender.Add(1)
-	go func(wgSender *sync.WaitGroup) {
-		defer wgSender.Done()
-		ln, err := net.Listen(transportProtocol, ":12348")
-		if err != nil {
-			assert.Fail(t, "failed to start test sender (Listen) ", err.Error())
-			return
-		}
-
-		connSender, err := ln.Accept()
-		if err != nil {
-			assert.Fail(t, "failed to start test sender (Accept) ", err.Error())
-			return
-		}
-
-		for _, d := range testData {
-			connSender.Write([]byte(d))
-		}
-	}(&wgSender)
-	defer wgSender.Wait()
+	wgListen.Add(1)
+	isListening.L.Lock()
+	go ListenAndServe(&wgListen, isListening, "", "12348", newConnections, shutdown)
+	isListening.Wait()
 
 	connReader, err := net.Dial(transportProtocol, ":12348")
 	if err != nil {
@@ -98,8 +85,16 @@ func TestReadConn(t *testing.T) {
 		return
 	}
 
-	var file, _ = connReader.(*net.TCPConn).File()
+	connSender := <-newConnections
+	for _, d := range testData {
+		_, err = connSender.Write([]byte(d))
+		if err != nil {
+			assert.Fail(t, "failed to send bytes sender (Write) ", err.Error())
+			return
+		}
+	}
 
+	var file, _ = connReader.(*net.TCPConn).File()
 	wgReader.Add(1)
 	go reader.ReadFile(&wgReader, file, messages, shutdown)
 
